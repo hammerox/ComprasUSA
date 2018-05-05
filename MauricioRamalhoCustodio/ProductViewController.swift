0c173718a792e2d80c4b04b08c50f27cc3291fbe
Usuário Convidado <Guest@pa06micro34.local>
2018-05-05 15:41:49 -0300

    ProductViewController: Added Toolbar and PickerView

--- MauricioRamalhoCustodio/ProductViewController.swift
+++ MauricioRamalhoCustodio/ProductViewController.swift
@@ -17,12 +17,14 @@ class ProductViewController: UIViewController {
     @IBOutlet weak var textPrice: UITextField!
     @IBOutlet weak var switchCreditCard: UISwitch!
     var product : String?
-    var imagePicker : UIImagePickerController()
+    var smallImage: UIImage!
+    var pickerView : UIPickerView!
+    var pickerData = ["Exemplo", "Mais um"]
+    var pickerSelectedRow = 0
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -42,7 +44,24 @@ class ProductViewController: UIViewController {
     }
     */
     @IBAction func selectImage(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar poster", message: "De onde você quer escolher o poster?", preferredStyle: .actionSheet)
         
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default, handler: { (action: UIAlertAction) in
+                self.selectPicture(sourceType: .camera)
+            })
+            alert.addAction(cameraAction)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func updateProduct(_ sender: UIButton) {
@@ -72,18 +91,112 @@ class ProductViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    @IBAction func setState(_ sender: UITextField) {
+        pickUp(textState)
+    }
     
     private func showAlert(message: String) {
-        let alert = UIAlertController(title: "Formulário incompleto", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: message, message: "", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
             NSLog("The \"OK\" alert occured.")
         }))
         self.present(alert, animated: true, completion: nil)
     }
     
+    
+    func selectPicture(sourceType: UIImagePickerControllerSourceType) {
+        //Criando o objeto UIImagePickerController
+        let imagePicker = UIImagePickerController()
+        
+        //Definimos seu sourceType através do parâmetro passado
+        imagePicker.sourceType = sourceType
+        
+        //Definimos a MovieRegisterViewController como sendo a delegate do imagePicker
+        imagePicker.delegate = self
+        
+        //Apresentamos a imagePicker ao usuário
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func pickUp(_ textField : UITextField){
+        
+        // UIPickerView
+        self.pickerView = UIPickerView(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        self.pickerView.backgroundColor = UIColor.white
+        textField.inputView = self.pickerView
+        let currentIndex = pickerData.index(of: textState.text!)?.hashValue
+        if currentIndex != nil {
+            pickerSelectedRow = currentIndex!
+            self.pickerView.selectRow(pickerSelectedRow, inComponent: 0, animated: false)
+        } else {
+            pickerSelectedRow = 0
+            self.pickerView.selectRow(0, inComponent: 0, animated: false)
+        }
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(ProductViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(ProductViewController.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneClick() {
+        self.textState.text = pickerData[pickerSelectedRow]
+        textState.resignFirstResponder()
+    }
+    
+    @objc func cancelClick() {
+        textState.resignFirstResponder()
+    }
+    
 }
 
 
-extension ProductViewController : UIImagePickerControllerDelegate {
-    // IMPLEMENTAR PICKER CONTROLLER
+extension ProductViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let smallSize = CGSize(width: 300, height: 280)
+        UIGraphicsBeginImageContext(smallSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+        
+        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        imagePicture.image = smallImage
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProductViewController : UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    //MARK:- PickerView Delegate & DataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerSelectedRow = row
+    }
+    
 }
